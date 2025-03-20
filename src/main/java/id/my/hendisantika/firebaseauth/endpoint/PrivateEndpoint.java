07524b23ae4cc13b56964e64e7f85ffbe49d671b
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 06:25:46 +0700

    feat: Add getUserInfo in PrivateEndpoint🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/firebaseauth/endpoint/PrivateEndpoint.java
+++ src/main/java/id/my/hendisantika/firebaseauth/endpoint/PrivateEndpoint.java
@@ -1,5 +1,9 @@
 package id.my.hendisantika.firebaseauth.endpoint;
 
+import id.my.hendisantika.firebaseauth.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("private")
 public class PrivateEndpoint {
+    @GetMapping("user-details")
+    public ResponseEntity<User> getUserInfo(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(user);
+    }
+
 }
