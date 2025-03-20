4bac5cb0382be926d68727820d42fd7b078c6e34
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 06:21:34 +0700

    feat: Add User Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/firebaseauth/model/User.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.firebaseauth.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-firebase-auth
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/03/25
+ * Time: 06.21
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class User implements Serializable {
+    private static final long serialVersionUID = 4408418647685225829L;
+    private String uid;
+    private String name;
+    private String email;
+    private boolean isEmailVerified;
+    private String issuer;
+    private String picture;
+}
