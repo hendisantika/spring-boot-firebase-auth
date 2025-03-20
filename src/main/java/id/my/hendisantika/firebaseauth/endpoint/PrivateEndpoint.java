be6a95aeb9af5ba1eb02e7a8aca61ad1bf722fb4
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 06:25:25 +0700

    feat: Add PrivateEndpoint🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/firebaseauth/endpoint/PrivateEndpoint.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.firebaseauth.endpoint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-firebase-auth
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/03/25
+ * Time: 06.25
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("private")
+public class PrivateEndpoint {
+}
