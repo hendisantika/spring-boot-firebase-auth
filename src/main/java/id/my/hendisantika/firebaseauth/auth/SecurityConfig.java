8ca71ffcbaaf25f563e46dff3c47aee65d4b2ba0
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 06:18:05 +0700

    feat: Add restAuthenticationEntryPoint in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/firebaseauth/auth/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/firebaseauth/auth/SecurityConfig.java
@@ -3,9 +3,17 @@ package id.my.hendisantika.firebaseauth.auth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +35,19 @@ public class SecurityConfig {
     public final SecurityFilter tokenAuthenticationFilter;
     private final ObjectMapper objectMapper;
     private final SecurityProperties restSecProps;
+
+    @Bean
+    public AuthenticationEntryPoint restAuthenticationEntryPoint() {
+        return (httpServletRequest, httpServletResponse, e) -> {
+            Map<String, Object> errorObject = new HashMap<>();
+            int errorCode = 401;
+            errorObject.put("message", "Unauthorized access of protected resource, invalid credentials");
+            errorObject.put("error", HttpStatus.UNAUTHORIZED);
+            errorObject.put("code", errorCode);
+            errorObject.put("timestamp", new Timestamp(new Date().getTime()));
+            httpServletResponse.setContentType("application/json;charset=UTF-8");
+            httpServletResponse.setStatus(errorCode);
+            httpServletResponse.getWriter().write(objectMapper.writeValueAsString(errorObject));
+        };
+    }
 }
