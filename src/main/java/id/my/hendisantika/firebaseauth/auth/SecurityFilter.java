df550e358735356a59330984e65233c808b17450
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 06:19:22 +0700

    feat: Add doFilterInternal in SecurityFilter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/firebaseauth/auth/SecurityFilter.java
+++ src/main/java/id/my/hendisantika/firebaseauth/auth/SecurityFilter.java
@@ -1,11 +1,17 @@
 package id.my.hendisantika.firebaseauth.auth;
 
 import id.my.hendisantika.firebaseauth.util.CookieUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,4 +37,10 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     private final SecurityProperties securityProps;
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        verifyToken(request);
+        filterChain.doFilter(request, response);
+    }
 }
