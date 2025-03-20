af7d7e0e9d05e84f2e6c861828346032d17362f6
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 06:15:04 +0700

    feat: Add CookieUtils🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/firebaseauth/util/CookieUtils.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.firebaseauth.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-firebase-auth
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/03/25
+ * Time: 06.14
+ * To change this template use File | Settings | File Templates.
+ */
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CookieUtils {
+
+    private final HttpServletRequest httpServletRequest;
+
+    private final HttpServletResponse httpServletResponse;
+
+    private final SecurityProperties restSecProps;
+
+}
