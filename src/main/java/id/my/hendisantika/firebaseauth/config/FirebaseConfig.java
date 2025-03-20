79e45e6bc543f04ad74e0df9581fa5fa677ca196
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 06:24:31 +0700

    feat: Add FirebaseConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/firebaseauth/config/FirebaseConfig.java
@@ -0,0 +1,42 @@
+package id.my.hendisantika.firebaseauth.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-firebase-auth
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/03/25
+ * Time: 06.23
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/notifikasi.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Firebase initialization failed", e);
+        }
+    }
+}
